028b4a1407d370fa0929ebf253ec53066b85a641
Tiafex <163532204+Tiafex@users.noreply.github.com>
2024-05-17 13:50:03 +0200

    Add files via upload

---
+++ Iacob/verifica/src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+     
+        /*
+         * 
+         Persone p = new Persone();
+         Botteghino b= new Botteghino();
+         
+         for (int i =0; i <50; i++)
+         {
+             Persone t1 =  new Persone(p,b);
+             p.start();
+             
+            }
+            */
+        
+
+
+
+    }
+}
---
+++ Iacob/verifica/src/main/java/com/example/Persone.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Persone extends Thread{
+
+ private Persona p1 = 50;   
+ private Botteghino b1;
+
+    public Persone(Persone p, Botteghino b) 
+    {
+        this.p1=p1;
+        this.b1=b1;
+
+
+
+    }
+
+   @Override
+   public void run()
+   {
+    
+   }
+
+
+}
